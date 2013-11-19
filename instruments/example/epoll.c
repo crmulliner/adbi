78d4aeefa9c8444000a39a1a0499f532e0400b66
collin <collin-github@mulliner.org>
2013-11-19 16:08:20 -0500

    initial release on github

---
+++ instruments/example/epoll.c
@@ -0,0 +1,90 @@
+/*
+ *  Collin's Binary Instrumentation Tool/Framework for Android
+ *  Collin Mulliner <collin[at]mulliner.org>
+ *  http://www.mulliner.org/android/
+ *
+ *  (c) 2012,2013
+ *
+ *  License: LGPL v2.1
+ *
+ */
+
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <dlfcn.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/select.h>
+#include <string.h>
+#include <termios.h>
+#include <pthread.h>
+#include <sys/epoll.h>
+
+#include <jni.h>
+#include <stdlib.h>
+
+#include "../base/hook.h"
+#include "../base/base.h"
+
+#undef log
+
+#define log(...) \
+        {FILE *fp = fopen("/data/local/tmp/adbi_example.log", "a+");\
+        fprintf(fp, __VA_ARGS__);\
+        fclose(fp);}
+
+
+// this file is going to be compiled into a thumb mode binary
+
+void __attribute__ ((constructor)) my_init(void);
+
+static struct hook_t eph;
+
+// for demo code only
+static int counter;
+
+// arm version of hook
+extern int my_epoll_wait_arm(int epfd, struct epoll_event *events, int maxevents, int timeout);
+
+/*  
+ *  log function to pass to the hooking library to implement central loggin
+ *
+ *  see: set_logfunction() in base.h
+ */
+static void my_log(char *msg)
+{
+	log(msg)
+}
+
+int my_epoll_wait(int epfd, struct epoll_event *events, int maxevents, int timeout)
+{
+	int (*orig_epoll_wait)(int epfd, struct epoll_event *events, int maxevents, int timeout);
+	orig_epoll_wait = (void*)eph.orig;
+
+	hook_precall(&eph);
+	int res = orig_epoll_wait(epfd, events, maxevents, timeout);
+	if (counter) {
+		hook_postcall(&eph);
+		log("epoll_wait() called\n");
+		counter--;
+		if (!counter)
+			log("removing hook for epoll_wait()\n");
+	}
+        
+	return res;
+}
+
+void my_init(void)
+{
+	counter = 3;
+
+	log("%s started\n", __FILE__)
+ 
+	set_logfunction(my_log);
+
+	hook(&eph, getpid(), "libc.", "epoll_wait", my_epoll_wait_arm, my_epoll_wait);
+}
+
